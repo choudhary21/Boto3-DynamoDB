8e13f499b8917f878d69f12628c4ec3568941787
Abhishek Choudhary <abhishek.choudhary@opcito.com>
2023-01-16 17:26:58 +0530

    add route for exporting to s3

--- src/dynamoDB_module/__init__.py
+++ src/dynamoDB_module/__init__.py
@@ -6,3 +6,6 @@ dynamoDB = Blueprint('dynamoDB', __name__, url_prefix = '/api/dynamodb')
 
 # route for enebling Point-In-Time-Restore
 dynamoDB.add_url_rule('/PITR', 'update_PITR', update_PITR, methods = ['PATCH'])
+
+# route for exporting table to s3
+dynamoDB.add_url_rule('/export', 'exportToS3', exportToS3, methods = ['PATCH'])
