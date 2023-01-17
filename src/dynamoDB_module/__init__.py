b5214923bb455fba245c717b7df7c11a2ec91744
Abhishek Choudhary <abhishek.choudhary@opcito.com>
2023-01-17 12:12:03 +0530

    added route for listing exports

--- src/dynamoDB_module/__init__.py
+++ src/dynamoDB_module/__init__.py
@@ -9,3 +9,6 @@ dynamoDB.add_url_rule('/PITR', 'update_PITR', update_PITR, methods = ['PATCH'])
 
 # route for exporting table to s3
 dynamoDB.add_url_rule('/export', 'exportToS3', exportToS3, methods = ['PATCH'])
+
+# route for listing all exported files
+dynamoDB.add_url_rule('/listexports', 'listExports', listExports)
