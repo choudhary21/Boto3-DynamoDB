2b5f2d60f3b3016a491c66a20b3256744622e8e9
kedar dhoble <kedardhoble2001@gmail.com>
2023-01-17 15:25:16 +0530

    added route for listing tables

--- src/dynamoDB_module/__init__.py
+++ src/dynamoDB_module/__init__.py
@@ -9,3 +9,6 @@ dynamoDB.add_url_rule('/PITR', 'update_PITR', update_PITR, methods = ['PATCH'])
 
 # route for exporting table to s3
 dynamoDB.add_url_rule('/export', 'exportToS3', exportToS3, methods = ['PATCH'])
+
+# route for listing all tables 
+dynamoDB.add_url_rule('/list', 'listTables', listTables, methods = ['GET'])
