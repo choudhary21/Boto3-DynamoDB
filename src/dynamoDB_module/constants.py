aaf9684b8084e34d45827a0b6a04916e1715e1fb
Abhishek Choudhary <abhishek.choudhary@opcito.com>
2023-01-19 19:31:00 +0530

    added constant for sqs puh message response

--- src/dynamoDB_module/constants.py
+++ src/dynamoDB_module/constants.py
@@ -1,3 +1,4 @@
 FAILED_VALIDATION = "Parameter validation failed"
 PITR_ENABALED = "Point-In-Time-Recovery enabled successfully"
 TABLE_EXPORTED = "Table successfully exported to s3"
+MESSAGE_PUSHED = "Message pushed successfully in SQS queue"
