a611a2cb14c57208a28f659d97c31d2e427ebef9
kedar dhoble <kedardhoble2001@gmail.com>
2023-02-02 16:20:15 +0530

    added DB connections to APIs and made some changes

--- src/dynamoDB_module/controllers.py
+++ src/dynamoDB_module/controllers.py
@@ -6,21 +6,32 @@ from .constants import *
 from flask import current_app
 from .service import validate_table
 from .service import validate_bucket
+from src.utils import create_client
+from .models import *
+from src.extensions import db
+
 # from .service import validate_maxRes
 
 # API for enabling the PITR for a table of DynamoDB
 def update_PITR():
     try:
         table = request.get_json()['table']
-    # Input validation for Table name in DynamoDB
-        validate_table(table) 
-        current_app.logger.info("Creating client instance for updatinf PITR")
+        current_app.logger.info("Creating client instance for updating PITR")
         client = boto3.client(
             'dynamodb', 
             aws_access_key_id = ACCESS_KEY, 
             aws_secret_access_key = SECRET_KEY, 
             region_name = REGION
             )
+
+        # Checking if PITR is already enabled
+        pitr_response = client.describe_continuous_backups(
+            TableName= table
+            )
+        isEnabled = pitr_response["ContinuousBackupsDescription"]["PointInTimeRecoveryDescription"]["PointInTimeRecoveryStatus"]
+        if isEnabled == "ENABLED":
+            return jsonify({"message" : PITR_ALREADY_ENABALED}), HTTPStatus.OK
+        # print("pitr: ", pitr_response["ContinuousBackupsDescription"]["PointInTimeRecoveryDescription"]["PointInTimeRecoveryStatus"])
         current_app.logger.info("Sending request to enable PITR")
 
         response = client.update_continuous_backups(
@@ -45,10 +56,10 @@ def exportToS3():
     status = False
     try:
         arn = request.get_json()["TableArn"]
-        table = request.get_json()["TableName"]
+        tableName = request.get_json()["TableName"]
     # Input validation for S3 Table name  
-        validate_table(table)
-        bucket = request.get_json()["bucket"]
+        validate_table(tableName)
+        bucket = request.get_json()["S3Bucket"]
     # Input validation for S3 Bucket name 
         validate_bucket(bucket)
         current_app.logger.info("Creating client instance for exporting to s3")
@@ -59,7 +70,7 @@ def exportToS3():
             region_name = REGION
             )
         table = client.describe_table(
-            TableName=table
+            TableName=tableName
             )
         table_arn = table['Table']['TableArn']        
 
@@ -68,7 +79,9 @@ def exportToS3():
             S3Bucket = bucket,
             ExportFormat="DYNAMODB_JSON"
         )
-
+        exporttoS3 = ExportToS3(tableName, bucket, response["ExportDescription"]["ExportTime"])
+        db.session.add(exporttoS3)
+        db.session.commit()
         return response
 
     except KeyError as missing:
@@ -80,23 +93,34 @@ def exportToS3():
 # API for List all the exported json files
 
 def listExports():
+    data = []
     try:
-    #     arn = request.get_json()["TableArn"]
-    #     maxRes = request.get_json()["Max"]        
-    # # Input validation for Maximum results  
-    #     validate_maxRes(maxRes)
+        bucket = request.get_json()["Bucket"]
+        # maxRes = request.get_json()["Max"]
         current_app.logger.info("Creating client instance for listing all exported files")
-        client = boto3.client(
-            'dynamodb', 
+        
+        client = boto3.resource(
+            's3', 
             aws_access_key_id = ACCESS_KEY, 
-            aws_secret_access_key = SECRET_KEY, 
+            aws_secret_access_key = SECRET_KEY,
             region_name = REGION
             )
-        response = client.list_exports(
-            # TableArn = arn,
-            # MaxResults = maxRes
-        )
-        return jsonify(response), HTTPStatus.OK
+        response = client.Bucket(bucket)
+        summaries = response.objects.all()
+        # summaries = summaries.split("/ ")
+        print("files", summaries)
+        if summaries == None: # condition if we get none response
+            raise Exception(NONE_VALUE)
+        for files in summaries:
+            # print("data: ", files)
+            if(files.key[-5:] == ".json"):
+                data.append(files.key[12:])
+
+        listExports = ListExports(bucket, data)
+        db.session.add(listExports)
+        db.session.commit()
+
+        return jsonify(data), HTTPStatus.OK
 
     except KeyError as missing:
         return {"error" : {"message" : FAILED_VALIDATION, "parameter" : str(missing)}}, HTTPStatus.BAD_REQUEST
@@ -106,13 +130,9 @@ def listExports():
 
 def listTables():
     try:
-        # tableName = request.get_json()["TableName"]
-        # limit = request.get_json()["Limit"]
-        # queueURL = request.get_json()["QueueUrl"]
-        # messageBody = request.get_json()["MessageBody"]
         current_app.logger.info("Creating client instance for listing all tables")
         client = boto3.client(
-            'dynamodb', 
+            'dynamodb',
             aws_access_key_id = ACCESS_KEY, 
             aws_secret_access_key = SECRET_KEY, 
             region_name = REGION
@@ -120,21 +140,30 @@ def listTables():
 
         response = client.list_tables()  
         # ExclusiveStartTableName = tableName, Limit = limit
-        
-        # queueURL = request.get_json()["QueueUrl"]
-        # messageBody = request.get_json()["MessageBody"]
-        # sqsClient = boto3.client(
-        #     'sqs',
-        #     aws_access_key_id = ACCESS_KEY,
-        #     aws_secret_access_key = SECRET_KEY,
-        #     region_name = REGION
-        # )    
+        queueName = request.get_json()["QueueName"]
 
-        # for table in response["TableNames"]:
-        #     sqsResponse = sqsClient.send_message(
-        #         QueueUrl = queueURL,
-        #         MessageBody = messageBody
-        #     )
+        sqsClient = boto3.client(
+            'sqs',
+            aws_access_key_id = ACCESS_KEY,
+            aws_secret_access_key = SECRET_KEY,
+            region_name = REGION
+        )    
+        queueName = sqsClient.get_queue_url(
+            QueueName=queueName)
+        queueURL = queueName['QueueUrl']
+
+        
+        for table in response["TableNames"]:
+            Tables = ListTables.query.filter_by(tableName=table).all()
+            sqsResponse = sqsClient.send_message(
+                QueueUrl = queueURL,
+                MessageBody = table
+            )
+            
+            if not Tables:
+                listTables = ListTables(table)
+                db.session.add(listTables)
+                db.session.commit()
 
         return jsonify(response["TableNames"]), HTTPStatus.OK
 
@@ -143,58 +172,3 @@ def listTables():
 
     except Exception as err:
         return jsonify({"error": str(err)}), HTTPStatus.BAD_REQUEST
-
-
-#  "TableName": "Movie",
-#     "Limit": 25,
-#     "MessageBody": "table",
-#     "QueueUrl": "https://sqs.us-east-1.amazonaws.com/300023816116/test"
-# def sendSQS():
-#     try:
-#         queueURL = request.get_json()["QueueUrl"]
-#         messageBody = request.get_json()["MessageBody"]
-#         # actualMessage = {"message" : messageBody}
-#         current_app.logger.info("Creating client to push message on SQS for table")
-#         client = boto3.client(
-#             'sqs', 
-#             aws_access_key_id = ACCESS_KEY, 
-#             aws_secret_access_key = SECRET_KEY, 
-#             region_name = REGION
-#             )
-
-#         response = client.send_message(
-#             QueueUrl=queueURL,
-#             MessageBody=json.dumps(messageBody)
-#         )
-
-#         return response
-
-#     except KeyError as missing:
-#         return {"error" : {"message" : FAILED_VALIDATION, "parameter" : str(missing)}}, HTTPStatus.BAD_REQUEST
-
-#     except Exception as err:
-#         return jsonify({"error": str(err)}), HTTPStatus.BAD_REQUEST
-
-# def recieveSQS():
-#     try:
-#         queueURL = request.get_json()["QueueUrl"]
-#         client = boto3.client(
-#             'sqs', 
-#             aws_access_key_id = ACCESS_KEY, 
-#             aws_secret_access_key = SECRET_KEY, 
-#             region_name = REGION
-#             )
-
-#         response = client.receive_message(
-#             QueueUrl=queueURL,
-#             MaxNumberOfMessages=10,
-#             WaitTimeSeconds=3
-#         )
-#         return response
-
-#     except KeyError as missing:
-#         return {"error" : {"message" : FAILED_VALIDATION, "parameter" : str(missing)}}, HTTPStatus.BAD_REQUEST
-
-#     except Exception as err:
-#         return jsonify({"error": str(err)}), HTTPStatus.BAD_REQUEST
-        
