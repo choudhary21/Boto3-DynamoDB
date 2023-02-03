79e4b13568cfebda16340614fc243c7f2e748d53
Abhishek Choudhary <abhishek.choudhary@opcito.com>
2023-02-03 15:33:39 +0530

    added some validations

---
+++ src/dynamoDB_module/service.py
@@ -0,0 +1,13 @@
+# # Input validation for Table name in DynamoDB
+def validate_table(table):
+    if not isinstance(table, str):
+        raise Exception("table name must be string")
+    elif len(table) < 3:
+        raise Exception("length of table name cannot be less than 3")
+
+# # Input validation for S3 Bucket name 
+def validate_bucket(bucket):
+    if not isinstance(bucket, str):
+        raise Exception("bucket name must be string")
+    elif len(bucket) < 3:
+        raise Exception("bucket must have length greater than or equal to 3")
