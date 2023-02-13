de6af45efb149c267b17d52f8c5b7fddbffcd08c
kedar dhoble <kedardhoble2001@gmail.com>
2023-02-13 11:17:20 +0530

    created common client and common resource method

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,20 @@
 import boto3
 from src.config import *
 
-def create_client(resource):
+def create_client(module):
     client = boto3.client(
-            resource, 
+            module, 
             aws_access_key_id = ACCESS_KEY, 
             aws_secret_access_key = SECRET_KEY, 
             region_name = REGION
             )
+    return client
+
+def create_resource(module):
+    resource = boto3.resource(
+        module,
+        aws_access_key_id = ACCESS_KEY, 
+        aws_secret_access_key = SECRET_KEY, 
+        region_name = REGION
+    )
+    return resource
