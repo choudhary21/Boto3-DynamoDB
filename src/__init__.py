857c007ab6666e374ec193fcc29c8302f24a820a
kedar dhoble <kedardhoble2001@gmail.com>
2023-02-13 11:19:35 +0530

    no essential changes

--- src/__init__.py
+++ src/__init__.py
@@ -2,15 +2,13 @@ from flask import Flask
 import logging
 from .dynamoDB_module import dynamoDB
 from .extensions import db
-
+from .dynamoDB_module.models import *
 
 app=Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://kedar:Kedar123@localhost/DynamoDB"
 db.init_app(app)
 
 with app.app_context():
-    from .dynamoDB_module.models import *
-    
     db.create_all()
 
 logging.basicConfig(filename='app.log', filemode='a', level=logging.INFO,
@@ -20,13 +18,11 @@ if app.debug:
     # Fix werkzeug handler in debug mode
     logging.getLogger('werkzeug').disabled = True
 
-
 app.register_blueprint(dynamoDB)
 
 @app.route("/",methods=["GET"])
 def index():
     return "Hello World"
     
-
 if __name__=="__main__":
     app.run(debug=True)
