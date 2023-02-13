bb14a8a91574819d998f24e3dc193f57c7648b63
Abhishek Choudhary <abhishek.choudhary@opcito.com>
2023-02-13 16:01:29 +0530

    created demo url and a method for list exports

--- src/dynamoDB_module/__init__.py
+++ src/dynamoDB_module/__init__.py
@@ -11,10 +11,12 @@ dynamoDB.add_url_rule('/PITR', 'update_PITR', update_PITR, methods = ['PATCH'])
 dynamoDB.add_url_rule('/export', 'exportToS3', exportToS3, methods = ['PATCH'])
 
 # route for listing all exported files
-dynamoDB.add_url_rule('/listexports', 'listExports', listExports)
+dynamoDB.add_url_rule('/listexports', 'listExports', listExports, methods = ['GET', 'POST'])
 
 
 dynamoDB.add_url_rule('/listtables', 'listTables', listTables)
 
 
 dynamoDB.add_url_rule('/pushmessage', 'pushMessages', pushMessages)
+
+dynamoDB.add_url_rule('/getPitr', 'getPitr', getPitr, methods = ['GET', 'POST'])
