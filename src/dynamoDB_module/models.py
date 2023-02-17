95eb4ee3821c1c480b4f2f6e5fcb2e3b9ba99cf0
Abhishek Choudhary <abhishek.choudhary@opcito.com>
2023-02-17 10:39:49 +0530

    updated the models list table & export to s3

--- src/dynamoDB_module/models.py
+++ src/dynamoDB_module/models.py
@@ -2,13 +2,13 @@ from src.extensions import db
 
 class ExportToS3(db.Model):
     _id = db.Column(db.Integer, primary_key = True)
-    TableName = db.Column(db.String(50))
+    TableId = db.Column(db.Integer)
     S3Bucket = db.Column(db.String(50))
-    ExportTime = db.Column(db.String(50))
-    def __init__(self, TableName, S3Bucket, ExportTime):
-        self.TableName = TableName
+    LocationString = db.Column(db.String(100))
+    def __init__(self, TableId, S3Bucket, LocationString):
+        self.TableId = TableId
         self.S3Bucket = S3Bucket
-        self.ExportTime = ExportTime
+        self.LocationString = LocationString
 
 class ListExports(db.Model):
     _id = db.Column(db.Integer, primary_key = True)
@@ -19,16 +19,18 @@ class ListExports(db.Model):
         self.Bucket=Bucket
 
 class ListTables(db.Model):
-    # _id = db.Column(db.Integer, primary_key = True)
-    tableName=db.Column(db.String(50), primary_key = True)
-    def __init__(self, tableName):
-        self.tableName=tableName
-
-class UpdatePITR(db.Model):
     _id = db.Column(db.Integer, primary_key = True)
-    tableName = db.Column(db.String(50))
-    pitr_status= db.Column(db.String(10))
-    def __init__(self, tableName, pitr_status):
-        self.tableName = tableName
-        self.pitr_status = pitr_status
+    Table=db.Column(db.String(50))
+    pitr_status = db.Column(db.Boolean)
+    def __init__(self, Table):
+        self.Table=Table
+        self.pitr_status = None
+
+# class UpdatePITR(db.Model):
+#     _id = db.Column(db.Integer, primary_key = True)
+#     tableName = db.Column(db.String(50))
+#     pitr_status= db.Column(db.String(10))
+#     def __init__(self, tableName, pitr_status):
+#         self.tableName = tableName
+#         self.pitr_status = pitr_status
 
