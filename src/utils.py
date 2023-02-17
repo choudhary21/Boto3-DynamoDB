60d64bc824024043c2ebf36a2a6bb18f9abccb92
Abhishek Choudhary <abhishek.choudhary@opcito.com>
2023-02-17 10:37:31 +0530

    adds methodsfor get id from name & success output

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import boto3
 from src.config import *
 from .dynamoDB_module.models import *
+from flask import jsonify
 
 def create_client(module):
     client = boto3.client(
@@ -22,9 +23,14 @@ def create_resource(module):
 
 def get_table(id):
     table = ListTables.query.filter_by(_id = id).first()
-    return table;
+    return table
 
 def get_table_id(name):
     table = ListTables.query.filter_by(Table = name).first()
-    return table._id;
+    return table._id
 
+def get_table_pitr(id):
+    pass
+
+def successful_output(message):
+    return jsonify({"message" : message})
